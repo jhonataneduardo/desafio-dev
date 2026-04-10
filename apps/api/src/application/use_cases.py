4b59d101c9ad169ecdf3507b0dd7059340335716
Jhonatan Eduardo <jhonatanepp@gmail.com>
2026-04-10 02:01:24 -0300

    feat: renomeia ImportTransactionsUseCase para TransactionRepositoryInterface

--- apps/api/src/application/use_cases.py
+++ apps/api/src/application/use_cases.py
@@ -1,9 +1,9 @@
 from application.dtos import InputTransactionDTO
-from domain.repositories import TransactionRepository
+from domain.repositories import TransactionRepositoryInterface
 
 
 class ImportTrasactionsUseCase:
-    def __init__(self, transaction_repository: TransactionRepository):
+    def __init__(self, transaction_repository: TransactionRepositoryInterface):
         self.transaction_repository = transaction_repository
 
     def execute(self, input_transactions: list[InputTransactionDTO]):
