be423cf0c94100d22507048eb105338413248364
Jhonatan Eduardo <jhonatanepp@gmail.com>
2026-04-14 01:04:59 -0300

    feat: implementa repositório de transações com métodos para persitência e busca

--- apps/api/src/domain/repositories.py
+++ apps/api/src/domain/repositories.py
@@ -1,13 +1,18 @@
+from typing import List
 from abc import ABC, abstractmethod
 
-from domain.entities import EntityTransaction
+from domain.entities import EntityTransaction, TransactionQuery
 
 
 class TransactionRepositoryInterface(ABC):
     @abstractmethod
-    def save(self, transaction: EntityTransaction):
+    def save(self, transaction: EntityTransaction) -> EntityTransaction:
         raise NotImplementedError
 
     @abstractmethod
-    def get_by_id(self, transaction_id: int) -> EntityTransaction:
+    def save_batch(self, transactions: List[EntityTransaction]) -> List[EntityTransaction]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def find_all(self, params: TransactionQuery) -> List[EntityTransaction]:
         raise NotImplementedError
